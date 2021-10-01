0d9e1ef627b90bf6dd75be8b3bbb010623cd57dd
Sutrisna <sutrisnaroy@tamu.edu>
2021-09-30 22:46:18 -0500

    book collection completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,6 +34,10 @@ class BooksController < ApplicationController
     end
   end
 
+  def delete
+    @book = Book.find(params[:id])
+  end
+
   # PATCH/PUT /books/1 or /books/1.json
   def update
     respond_to do |format|
@@ -49,11 +53,12 @@ class BooksController < ApplicationController
 
   # DELETE /books/1 or /books/1.json
   def destroy
+    @book = Book.find(params[:id])
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    # respond_to do |format|
+    #   format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
@@ -64,6 +69,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title)
+      params.require(:book).permit(:title, :author, :price, :published_date)
     end
 end
--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,56 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'tris'
+    fill_in 'Price', with: 1
+    fill_in 'Published date', with: Date.new(2021, 10, 01)
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('tris')
+    expect(page).to have_content(1)
+    expect(page).to have_content('2021-10-01')
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter 2'
+    fill_in 'Author', with: 'tris 2'
+    fill_in 'Price', with: 10
+    fill_in 'Published date', with: Date.new(2001, 10, 01)
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter 2')
+    expect(page).to have_content('tris 2')
+    expect(page).to have_content(10)
+    expect(page).to have_content('2001-10-01')
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter 3!'
+    fill_in 'Author', with: 'tri!s 3'
+    fill_in 'Price', with: 100
+    fill_in 'Published date', with: Date.new(2021, 10, 9)
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter 3!')
+    expect(page).to have_content('tri!s 3')
+    expect(page).to have_content(100)
+    expect(page).to have_content('2021-10-09')
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: '4 .harry potter'
+    fill_in 'Author', with: 'tris /'
+    fill_in 'Price', with: 18042
+    fill_in 'Published date', with: Date.new(2001, 12, 10)
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('4 .harry potter')
+    expect(page).to have_content('tris /')
+    expect(page).to have_content(18042)
+    expect(page).to have_content('2001-12-10')
   end
 end
